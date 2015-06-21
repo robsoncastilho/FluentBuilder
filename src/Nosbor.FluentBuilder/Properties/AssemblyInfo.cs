ea3db8de37a15cdc9059e3767418b890e9ddae57
Robson Castilho <robsoncastilho@yahoo.com.br>
2015-06-20 21:20:55 -0400

    NuSpec para geracao de pacote nuget no servidor de build

--- src/Nosbor.FluentBuilder/Properties/AssemblyInfo.cs
+++ src/Nosbor.FluentBuilder/Properties/AssemblyInfo.cs
@@ -6,7 +6,7 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("FluentBuilder")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Create objects for testing purpose in a fluent and easy way.")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Robson Castilho")]
 [assembly: AssemblyProduct("FluentBuilder")]
